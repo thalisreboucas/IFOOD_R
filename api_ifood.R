3247e36076cf230bc0235c44edde057713b22298
Thalis Rebouças de Oliveira <50429343+thalisreboucas@users.noreply.github.com>
2023-07-19 16:57:14 -0300

    Add files via upload

--- api_ifood.R
+++ api_ifood.R
@@ -24,3 +24,36 @@ return(content)
 }
 
 UserCode(clientId)
+
+
+token <- function(clientId,clientSecret){
+  # Define the URL and parameters
+  url <- "https://merchant-api.ifood.com.br/authentication/v1.0/oauth/token"
+  headers <- c(
+    "accept" = "application/json",
+    "Content-Type" = "application/x-www-form-urlencoded"
+  )
+  data <- list(grantType = "client_credentials",
+               clientId = clientId[1],
+               clientSecret = clientSecret[1],
+               authorizationCode = "",
+               authorizationCodeVerifier = "",
+               refreshToken = ""
+               )
+  
+  # Make the POST request
+  response <- httr::POST(url, httr::add_headers(.headers=headers), body = data, encode = "form")
+  
+  # Check the status code of the response
+  status_code <- httr::status_code(response)
+  
+  # Check the content of the response
+  content <- httr::content(response)
+  
+  return(content)
+}
+
+clientId = "35e0b0f4-6f91-473d-acd1-98b7b310924e"
+clientSecret = "fx7vre8dxhpyztxdxmoyvwu5nv9zp9pi92eji4b72fmevu67npnvkura2xwh9lzlu2hast5e6m9vfnpkp12efmxsbha38n7y9c2"
+
+token(clientId,clientSecret)
